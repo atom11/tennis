a31ad5da27013303f5b72b406b97d9eeac888c2e
atom11 <rdewolf@Robins-MacBook-Pro.local>
2015-06-10 22:11:27 -0700

    v1

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+$('h1').lettering();
+
+var audio = $("#sound-1")[0];
+$(".char1").mouseenter(function() {
+  audio.play();
+});
+
+var audio2 = $("#sound-2")[0];
+$(".char3").mouseenter(function() {
+  audio2.play();
+});
+
+var audio3 = $("#sound-3")[0];
+$(".char5").mouseenter(function() {
+  audio3.play();
+});
+
+var audio4 = $("#sound-4")[0];
+$(".char7").mouseenter(function() {
+  audio4.play();
+});
+
+var audio5 = $("#sound-5")[0];
+$(".char9").mouseenter(function() {
+  audio5.play();
+});
+
+var audio6 = $("#sound-6")[0];
+$(".char11").mouseenter(function() {
+  audio6.play();
+});
+
+var audio7 = $("#sound-7")[0];
+$(".char13").mouseenter(function() {
+  audio7.play();
+});
